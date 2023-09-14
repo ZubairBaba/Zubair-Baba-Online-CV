b1549191354c83657932064e6c8de90d98c7665e
ZubairBaba <144887205+ZubairBaba@users.noreply.github.com>
2023-09-14 10:24:24 +0200

    Update update.js

--- js/update.js
+++ js/update.js
@@ -11,6 +11,8 @@ ul.addEventListener("click", function(event) {
           window.location.href = "Skills.html";
         } else if (selectedLink === "Linkedin") {
           window.location.href = "https://www.linkedin.com/in/zubair-baba-1aaa8126a";
+        } else if (selectedLink === "Contact Details") {
+          window.location.href = "Contact.html";
         }
         event.preventDefault(); // Prevent the default link behavior (navigating to the URL)
     }
